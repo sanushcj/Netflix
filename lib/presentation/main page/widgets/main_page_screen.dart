2bbdb2c06f29f36f1de1bc2daf3c71719c5a2ff5
Sanush CJ <sanushcj@gmail.com>
2022-12-06 18:37:32 +0530

    folders added

---
+++ lib/presentation/main page/widgets/main_page_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+class MainPage extends StatelessWidget {
+const MainPage({super.key, });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
