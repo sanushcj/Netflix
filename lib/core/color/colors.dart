711f54617ad6947b6cbb618ebf075caec7423144
Sanush CJ <sanushcj@gmail.com>
2022-12-07 16:57:29 +0530

    working on ownload page

--- lib/core/color/colors.dart
+++ lib/core/color/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 const backgroundColor = Colors.black;
-const whiteColor = Colors.white;
+const rrwhiteColor = Colors.white;
+const rrbuttoncolor = Colors.blue;
--- lib/presentation/downloads/widgets/download_screen.dart
+++ lib/presentation/downloads/widgets/download_screen.dart
@@ -1,17 +1,71 @@
-
 import 'package:flutter/material.dart';
+import 'package:netflix/core/color/colors.dart';
+import 'package:netflix/core/constants/constants_.dart';
 import 'package:netflix/presentation/widgets/app_bar_widgets.dart';
+
 class DownloadScreen extends StatelessWidget {
   const DownloadScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    final Size size = MediaQuery.of(context).size;
+    return SafeArea(
       child: Scaffold(
-        
-        appBar: PreferredSize(preferredSize: const Size.fromHeight(50), child: AppBArWidget(title: 'Downloads',)),
-        body: const Text('Screen Downloads'),
-      ),
+          appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(50),
+              child: AppBArWidget(
+                title: 'Downloads',
+              )),
+          body: ListView(
+            children: [
+              Row(
+                children: const [
+                  kwidth,
+                  Icon(
+                    Icons.settings,
+                    color: rrwhiteColor,
+                  ),
+                  kwidth,
+                  Text('Smart Downloads'),
+                ],
+              ),
+              const Text('Indroducing Downloads For You'),
+              const Text(
+                  'We will downloads for you we will download personalized selection of the movies and shows for yo, so there is always something to watch on your device'),
+              Container(
+                width: size.width,
+                height: size.height / 2,
+                color: rrwhiteColor,
+                child: Stack(
+                  children: [
+                    Center(child: CircleAvatar(radius: size.width * 0.36))
+                  ],
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: rrbuttoncolor,
+                child: const Text(
+                  'Set UP',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: rrwhiteColor),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: rrwhiteColor,
+                child: const Text(
+                  'See what you can download ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: backgroundColor),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
