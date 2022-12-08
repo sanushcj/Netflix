0e750b73253b49e47e04a13e168a713e67d405ed
Sanush CJ <sanushcj@gmail.com>
2022-12-08 19:15:19 +0530

    dummy search screen done

--- lib/presentation/search/widgets/search_result.dart
+++ lib/presentation/search/widgets/search_result.dart
@@ -8,7 +8,7 @@ class SearchResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         kheight,
         Search_Title(
