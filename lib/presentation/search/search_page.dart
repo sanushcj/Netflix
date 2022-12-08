490d92e449e755564be71b1c82f1a9ee5a8d64e3
Sanush CJ <sanushcj@gmail.com>
2022-12-08 10:12:56 +0530

    Search page started

--- lib/presentation/search/search_page.dart
+++ lib/presentation/search/search_page.dart
@@ -1,12 +1,29 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/core/color/colors.dart';
+
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Search'),
-    );
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+          CupertinoSearchTextField(
+            backgroundColor: rrGreyColor.withOpacity(0.4),
+            prefixIcon: const Icon(
+              CupertinoIcons.search,
+              color: rrGreyColor,
+            ),
+            suffixIcon: const Icon(CupertinoIcons.xmark_circle_fill,color: rrGreyColor,),style: TextStyle(color: rrwhiteColor),
+          ),
+        ],
+      ),
+    )));
   }
 }
