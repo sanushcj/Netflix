51fe209572be28ec6697a099b67fcf717a034f4b
Sanush CJ <sanushcj@gmail.com>
2022-12-12 14:14:10 +0530

    dummy new and  screen started

--- lib/presentation/new_and hot/new_and_hot_page.dart
+++ lib/presentation/new_and hot/new_and_hot_page.dart
@@ -1,12 +1,44 @@
-
 import 'package:flutter/material.dart';
+import 'package:netflix/core/color/colors.dart';
+
+import '../widgets/app_bar_widgets.dart';
+
 class NewandHotScreen extends StatelessWidget {
-   NewandHotScreen({super.key});
+  const NewandHotScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Screen Hot '),
-    );
+    return DefaultTabController(
+      length: 2,
+      child:
+       SafeArea(
+          child: Column(
+            children: [
+              AppBArWidget(title: 'Hot & New'),
+              TabBar(
+                  isScrollable: true,
+                  indicator: BoxDecoration(
+                      color: rrwhiteColor,
+                      borderRadius: BorderRadius.circular(30)),
+                  labelColor: backgroundColor,
+                  labelStyle: const TextStyle(
+                      color: backgroundColor,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
+                  unselectedLabelColor: rrwhiteColor,
+                  tabs: const [
+                    Tab(
+                      text: '🍿 Coming Soon',
+                    ),
+                    Tab(
+                      text: "👀 Everyone's Watching",
+                    ),
+                    
+                  ]),
+                  // TabBarView(children: _buid)
+            ],
+          ),
+        ),
+      );
   }
 }
